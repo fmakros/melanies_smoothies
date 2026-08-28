2400d252ef1bf502fc91a844fd601f8073056699
fmakros <fritz.meister@akros.ch>
2026-08-28 13:02:09 +0200

    Imported files from snowflake

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom smoothie
+  """
+)
+
+# Get the current credentials
+session = get_active_session()
+
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+
+name = st.text_input("Name on Smoothie:", "")
+
+ingredient_list = st.multiselect("Choose up to 5 ingredients", my_dataframe, max_selections=5)
+
+
+
+if ingredient_list:
+
+    ingredients_string = ' '.join(ingredient_list)
+
+    time_to_insert = st.button("Submit Order!")
+    if time_to_insert:
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                        values ('""" + ingredients_string + """', '""" + name + """')"""
+        #st.write(my_insert_stmt)
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name}!', icon="✅")
